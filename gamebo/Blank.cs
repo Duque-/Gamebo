15af1a5eba0a0a9593ac199e251a55c519440c12
Duque- <duquedepata@gmail.com>
2018-04-20 22:39:41 +1200

    Start

---
+++ gamebo/Blank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamebo {
+    class Blank {
+        protected int ID;
+        public string name;
+    }
+}
---
+++ gamebo/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamebo {
+    class Player: Blank{
+        public int experience;
+
+        public Player() {
+            name = "Duque";
+            experience = 0;
+        } 
+    }
+}
